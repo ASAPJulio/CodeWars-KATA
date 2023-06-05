25c99658da33b84da4175bdd2a6fe395478a0c9e
Jules_T <juliosarfyan@gmail.com>
2023-06-05 11:58:18 +0200

    feat: new kata opposite_number completed with soluce

---
+++ opposite_number.js
@@ -0,0 +1,11 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+function opposite(number) {
+// Your code here
+  };
+
+// Good Answer 
+
+function opposite(number) {
+    return (-number)
+  };
