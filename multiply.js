cec2fef9c8a35e219b3ed08d575674717bad21a7
Jules_T <juliosarfyan@gmail.com>
2023-06-05 11:13:33 +0200

    feat: Multiply.js completed and even_or_odd.js to

--- multiply.js
+++ multiply.js
@@ -2,6 +2,9 @@
 
 // This code does not execute properly. Try to figure out why
 
-function multiply (a,b){
+function multiply (a,b)
     a * b
-}
+
+// Good answer
+function multiply(a, b) { 
+  return a * b }
